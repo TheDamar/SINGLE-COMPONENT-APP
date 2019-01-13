d610fd5e46603b6e5fc4d9dc8b256ef8f66afda7
TheDamar <46629399+TheDamar@users.noreply.github.com>
2019-01-13 20:28:52 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.jpeg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+        Helloe the world</p>
+       
+        <br>
+        
+        </br>
+        
+        <hr>
+        </hr>
+        
+        
+        
+        
+        <p class = "paragraphe">
+        Salut Tous Le Monde</p>
+        
+        
+      </div>
+    );
+  }
+}
+export default App;
